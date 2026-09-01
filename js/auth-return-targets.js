cc2c80113a757604ee84c39fc8f400fd257764b3
Alexis Sacco <firstvololearning@gmail.com>
2026-08-31 22:40:32 -0400

    Add Story Builder account return target

--- js/auth-return-targets.js
+++ js/auth-return-targets.js
@@ -1,6 +1,6 @@
 (function () {
   "use strict";
-  const targets = Object.freeze({ storyBuilder: null, morphology: "http://127.0.0.1:8767/index.html", primoVolo: null });
+  const targets = Object.freeze({ storyBuilder: "https://firstvololearning-ctrl.github.io/First-Volo-Story-Builder/", morphology: "http://127.0.0.1:8767/index.html", primoVolo: null });
   function requestedKey() { const key = new URLSearchParams(window.location.search).get("returnTo"); return Object.prototype.hasOwnProperty.call(targets, key) ? key : null; }
   function destinationFor(key) { return targets[key] || null; }
   window.FirstVoloAccountReturnTargets = { requestedKey, destinationFor };
