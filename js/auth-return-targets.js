ae4932f5dbd411c3919d5754b5e93a6d1f00e535
Alexis Sacco <firstvololearning@gmail.com>
2026-09-01 20:27:33 -0400

    Add Morphology student progress link

--- js/auth-return-targets.js
+++ js/auth-return-targets.js
@@ -1,7 +1,10 @@
 (function () {
   "use strict";
   const targets = Object.freeze({ storyBuilder: "https://firstvololearning-ctrl.github.io/First-Volo-Story-Builder/", morphology: "https://firstvololearning-ctrl.github.io/First-Volo-Morphology/", primoVolo: "https://firstvololearning-ctrl.github.io/Primo-Volo-Italian-Learning-Hub/" });
+  const localHarness = window.location.hostname === "127.0.0.1" || window.location.hostname === "localhost";
+  const detailedProgressTargets = Object.freeze({ morphology: localHarness ? `${window.location.origin}/morphology/program-progress.html` : `${targets.morphology}program-progress.html` });
   function requestedKey() { const key = new URLSearchParams(window.location.search).get("returnTo"); return Object.prototype.hasOwnProperty.call(targets, key) ? key : null; }
   function destinationFor(key) { return targets[key] || null; }
-  window.FirstVoloAccountReturnTargets = { requestedKey, destinationFor };
+  function detailedProgressFor(key) { return detailedProgressTargets[key] || null; }
+  window.FirstVoloAccountReturnTargets = { requestedKey, destinationFor, detailedProgressFor };
 }());
